7fead5d54267361878348da3adfbb61a5a407e30
jcsilva7 <josemiguelcsilva@gmail.com>
2026-06-25 21:59:28 +0100

    Added personalisation from a json to reduce token usage

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
+import json
 import os
 import discord
 import aiohttp
-import sys
+import argparse
 from dotenv import load_dotenv
 from console_log import console_log
 
@@ -12,9 +13,15 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+# Context related variables
 ENABLE_CONTEXT = False
-CONTEXT_WINDOW_SIZE = 0
+CONTEXT_WINDOW_SIZE = -1
 CONTEXT_WINDOW = []
+
+# Person specific info variables
+# Allow personalised info (from a JSON) for different people to reduce token usage
+# The info is only fetched if the person (a key) is mentioned in the message
+ENABLE_PERSONALISED = False
 
 async def get_model_response(message: discord.Message) -> str|None:
     API_KEY = os.getenv("API_KEY")
@@ -36,21 +43,34 @@ async def get_model_response(message: discord.Message) -> str|None:
     with open("instructions.txt", 'r') as f:
         instructions = f.read().strip()
 
+    prompt = f"{instructions}"
+
     if ENABLE_CONTEXT:
-        prompt = f"""
-            {instructions}
+        prompt += f"""
             These are the {CONTEXT_WINDOW_SIZE} previous messages and answers
             in the format "User_Nickname: user_message\nModel_response":
             {CONTEXT_WINDOW}
+        """
+
+    if ENABLE_PERSONALISED:
+        with open("info.json", "r") as info_file:
+            data = json.load(info_file)
+
+        # Check who is mentioned
+        matches = [key for key in data.keys() if key in content]
+
+        if matches:
+            prompt += "\nThis is personalized info from people mentioned, make sure to include it\n"
+            for match in matches:
+                prompt += f"""
+                        {match} personal info:
+                        {data[match]}
+                    """
+
+    prompt += f"""
             This is the message by {nick}:
             {content}
-        """
-    else:
-        prompt = f"""
-            {instructions}
-            This is the message by {nick}:
-            {content}
-        """
+    """
 
     payload = {
         # CHANGE MODEL HERE
@@ -82,11 +102,11 @@ async def get_model_response(message: discord.Message) -> str|None:
                 data = await resp.json()
                 response = data["choices"][0]["message"]["content"].strip()
 
-                CONTEXT_WINDOW.append(f"{nick}: {content}\n{response}")
-
                 # Cut previous messages
-                if len(CONTEXT_WINDOW) > CONTEXT_WINDOW_SIZE:
-                    CONTEXT_WINDOW.pop(0)
+                if ENABLE_CONTEXT:
+                    CONTEXT_WINDOW.append(f"{nick}: {content}\n{response}")
+                    if len(CONTEXT_WINDOW) > CONTEXT_WINDOW_SIZE:
+                        CONTEXT_WINDOW.pop(0)
 
                 return response
 
@@ -135,24 +155,45 @@ async def on_message(message):
         return
 
 def main():
-    global CONTEXT_WINDOW_SIZE, ENABLE_CONTEXT
+    global CONTEXT_WINDOW_SIZE, ENABLE_CONTEXT, ENABLE_PERSONALISED
+
+    parser = argparse.ArgumentParser("Discord AI Chat Bot")
 
     try:
-        if len(sys.argv) > 1:
-            CONTEXT_WINDOW_SIZE = int(sys.argv[1])
-            if CONTEXT_WINDOW_SIZE <= 0:
-                raise Exception("Context window size must be greater than 0")
+        parser.add_argument("--context-window-size", type=int, default=CONTEXT_WINDOW_SIZE,
+                            help="Number of messages in the chatbot context window, must include an integer as argument")
+        parser.add_argument("--personalized", action="store_true",
+                            help="Enabled personalized info for users fetched from a json file "
+                                 "with users as keys and descriptions as values")
+
+        args = parser.parse_args()
+
+        if args.context_window_size != -1:
+            if args.context_window_size <= 0:
+                raise Exception("Context window size must be above 0")
+
             ENABLE_CONTEXT = True
+            CONTEXT_WINDOW_SIZE = args.context_window_size
+
+        if args.personalized:
+            if not os.path.exists("info.json"):
+                raise Exception("Personalized info file (info.json) not found")
+
+            ENABLE_PERSONALISED = True
 
         if DISCORD_TOKEN is None:
             raise Exception("No Discord token provided")
 
+        if not os.path.exists("instructions.txt"):
+            raise Exception("Instructions file not found")
+
         client.run(DISCORD_TOKEN)
     except ValueError:
-        console_log("Wrong arguments: python3 bot.py {Context Window or Nothing if disabled}")
+        console_log("Wrong arguments (leave blank if disabled): python3 bot.py {Context Window}")
         return
     except Exception as e:
         console_log(str(e))
+        return
 
 if __name__ == '__main__':
     main()
